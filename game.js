b98896ad07150281fd0672a8992200ec284ff7c9
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2024-04-15 18:56:06 -0300

    add sound

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ function nextSequence (){
     return randomNumber;
 }
 
+/*
 var randomChosenColour = buttonColours[nextSequence()];
 
 gamePattern.push(randomChosenColour);
@@ -13,15 +14,40 @@ gamePattern.push(randomChosenColour);
 //console.log(gamePattern);
 
 //selecting the element
+
 var colorId = "#"+randomChosenColour;
 var buttonToAnimate = $(colorId);
-buttonToAnimate.css("transform", "scale(0.9)"); // Reduz o tamanho do botão
+buttonToAnimate.css("transform", "scale(0)"); // Reduz o tamanho do botão
 setTimeout(function(){
     buttonToAnimate.css("transform", "scale(1)"); // Retorna ao tamanho original
 },50);
 
+//criando e tocando o elemento audio de acordo com a cor :
 
-/*
+function getAudioSource (color){
+    switch (color) {
+        case "red":
+            return "./sounds/red.mp3"
+            break;
+        case "blue":
+            return "./sounds/blue.mp3"
+            break;
+        case "yellow":
+            return "./sounds/yellow.mp3"
+            break;
+        case "green":
+            return "./sounds/green.mp3"
+            break;
+        default:
+            break;
+    }
+}
+
+var source = getAudioSource(randomChosenColour);
+var audioElement = $("<audio>").attr("src", source).get(0);
+audioElement.play();
+*/
+
 $("#green").click(function(e){
     
     var randomChosenColour = buttonColours[nextSequence()];
@@ -37,6 +63,27 @@ $("#green").click(function(e){
     setTimeout(function(){
         buttonToAnimate.css("transform", "scale(1)"); // Retorna ao tamanho original
     },50);
-})
 
-*/
+    function getAudioSource (color){
+        switch (color) {
+            case "red":
+                return "./sounds/red.mp3"
+                break;
+            case "blue":
+                return "./sounds/blue.mp3"
+                break;
+            case "yellow":
+                return "./sounds/yellow.mp3"
+                break;
+            case "green":
+                return "./sounds/green.mp3"
+                break;
+            default:
+                break;
+        }
+    }
+
+    var source = getAudioSource(randomChosenColour);
+    var audioElement = $("<audio>").attr("src", source).get(0);
+    audioElement.play();
+});
