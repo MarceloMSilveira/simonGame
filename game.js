22e768b7656e9733c825e39173b944584365e316
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2024-04-16 11:46:03 -0300

    final changes

--- game.js
+++ game.js
@@ -4,6 +4,12 @@ var userClickedPattern = [];
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+function startOver(params) {
+    gameBegin=true;
+    level=0;
+    gamePattern=[];
+}
+
 function animatePress(currentColor) {
     var colorId = "#"+currentColor;
     var buttonToAnimate = $(colorId);
@@ -27,6 +33,9 @@ function getAudioSource (color){
         case "green":
             return "./sounds/green.mp3"
             break;
+        case "wrong":
+            return "./sounds/wrong.mp3"
+            break;
         default:
             break;
     }
@@ -38,12 +47,45 @@ function playSound(colorName) {
     audioElement.play();
 }
 
+function showEfects(colorBtn){
+    animatePress(colorBtn);
+    playSound(colorBtn);
+}
+
 function nextSequence (){
+    userClickedPattern=[];
     //seleciona nr aleatório entre 0 e 3
     var randomNumber = Math.floor(4*Math.random());
-    $("h1").text("Level "+level);
+    //associa o nr a uma cor 
+    var randomChosenColour = buttonColours[randomNumber];
+    //armazena a nova cor
+    gamePattern.push(randomChosenColour);
+    showEfects(randomChosenColour);
     level++;
-    return randomNumber;
+    $("h1").text("Level "+level);
+    console.log(gamePattern);
+}
+
+function checkAnswer(currentLevel) {
+    var i = currentLevel;
+    if (userClickedPattern[i]===gamePattern[i]){
+        console.log("success");
+        if (userClickedPattern.length===gamePattern.length){
+            setTimeout(() => {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        }, 200);
+        console.log("wrong");
+        $("h1").text("Game-over, press any key to restart.");
+        startOver();
+    }
+    
 }
 
 $(".btn").click(function(e){
@@ -51,27 +93,20 @@ $(".btn").click(function(e){
     playSound(userChosenColour);
     animatePress(userChosenColour);
     userClickedPattern.push(userChosenColour);
-    //console.log(userClickedPattern);
+    console.log(userClickedPattern);
+    var indexOfLastAnswer = userClickedPattern.length-1;
+    checkAnswer(indexOfLastAnswer);
 });
 
 //start game when any key is pressed:
 $(document).keypress(function () {
     if (gameBegin){
-        //show current level (level 0):
-        $("h1").text("Level "+level);
-        //identifica a cor aleatoriamente escolhida
-        //chama nextSequence que (muda o texto para identificar a fase)
-        var randomChosenColour = buttonColours[nextSequence()];
-        
-        //toca som do botão escolhido:
-        playSound(randomChosenColour);
-        //anima o botão escolhido:
-        animatePress(randomChosenColour);
-        //coloca a cor dentro do array de padrões 
-        //escolhidos pelo jogo
-        gamePattern.push(randomChosenColour);
+        $("h1").text("Level 0");
 
+        nextSequence();
+        
+        
         //configura flag para executar somente na primeira vez
         gameBegin=false;
     }
-})
+}) 
